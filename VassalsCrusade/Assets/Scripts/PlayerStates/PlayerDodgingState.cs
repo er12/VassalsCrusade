dff4590478cd0d98a7fef7776d1b09bcc32e0192
Ernesto Rodriguez <ernestorod27@gmail.com>
2020-12-30 00:38:50 -0400

    Basic Player movility and sprites import
    Made some tutorials and included  finite state machine to player

---
+++ VassalsCrusade/Assets/Scripts/PlayerStates/PlayerDodgingState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDodgingState : PlayerBaseState
+{
+    private Vector3 dodgeDirection;
+    public float dodgeSpeed;
+
+    public override void EnterState(PlayerController player)
+    {
+        dodgeDirection = player.WalkingState.Movement;
+        dodgeSpeed = 35f;
+
+        player.Animator.SetBool("Dodging", true);
+    }
+
+    public override void Update(PlayerController player)
+    {
+
+        float dodgeSpeedDropMultiplier = 7f;
+        dodgeSpeed -= dodgeSpeed * dodgeSpeedDropMultiplier * Time.deltaTime;
+
+        float dodgeSpeedMinimun = 7f;
+
+        //Finished dodgeing
+        if (dodgeSpeed < dodgeSpeedMinimun)
+        {
+            player.Animator.SetBool("Dodging", false);
+            player.TransitionToState(player.WalkingState);
+        }
+
+    }
+
+    public override void FixUpdate(PlayerController player)
+    {
+        player.Rigidbody.velocity = dodgeDirection * dodgeSpeed;
+    }
+}
+
---
+++ VassalsCrusade/Assets/Scripts/PlayerStates/PlayerIdleState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    public override void EnterState(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FixUpdate(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(PlayerController player)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
